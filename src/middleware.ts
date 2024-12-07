ec39ba837cbbb1f63980402680f47059d5967aab
msjace <ms7832jace@gmail.com>
2024-12-07 11:15:45 +0900

    ignore toast map

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,19 @@ import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
-export const middleware = (request: NextRequest): NextResponse => {
-  return NextResponse.redirect(new URL('/todos', request.url))
-}
+const igorePaths = [
+  '/_next/static/css/ReactToastify.css.map',
+  '/_next/static/react-toastify.esm.mjs.map',
+]
 
-export const config = {
-  matcher: ['/'],
+export const middleware = (request: NextRequest): NextResponse => {
+  const url = request.nextUrl
+  if (igorePaths.some((path) => url.pathname.startsWith(path))) {
+    return NextResponse.next()
+  }
+  if (url.pathname === '/') {
+    return NextResponse.redirect(new URL('/todos', request.url))
+  }
+
+  return NextResponse.next()
 }
